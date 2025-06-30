bf2efb84f094d422c489324c07d1de5b44f636c4
Gonzalezedgar1992 <edgardo.gonzalez.h@usach.cl>
2025-06-29 22:16:57 -0400

    Subiendo comentarios al codigo

--- prueba_git.py
+++ prueba_git.py
@@ -1,8 +1,12 @@
+#Nombre paquete
 import cv2
+#abrir imagen 
 img= cv2.imread('panel-solar.jpg')
+# utilizar Canny
 bordeCanny=cv2.Canny(img, 50,100)
+#Graficar imagen original y bordes
 cv2.imshow('Original', img)
 cv2.imshow('Canny - min=100, max=200', bordeCanny)
-
+#presionar tecla para dejar de ejecutar
 cv2.waitKey(0)
 cv2.destroyAllWindows()
